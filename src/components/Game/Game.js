79e56c2bba72bc5883e7673a88f6ba1526efa0e5
Your Name <phamtienquan2001@gmail.com>
2022-10-26 19:07:12 +0700

    .

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -105,7 +105,7 @@ const Game=()=>{
 
   const moves = history.map((step, move) => {
     const desc = move ?
-      `Go to move # ${move}, position: `:
+      `Go to move # ${move} `:
       'Go to game start';
     return (
       <li key={move}>
